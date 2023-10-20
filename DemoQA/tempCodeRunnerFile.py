9d03834aba8fccb82ffc8e48acedd51e2cef9f40
irsyadamri <irsyadamri52@gmail.com>
2023-10-20 16:16:07 +0700

    firts commit

--- DemoQA/tempCodeRunnerFile.py
+++ DemoQA/tempCodeRunnerFile.py
@@ -1,3 +1,3 @@
-driver.find_element(By.XPATH, '//*[@id="alertButton"]').click()
-# time.sleep(2)
-# driver.switch_to.alert.accept()
+driver.find_element(By.XPATH, '//*[@id="confirmButton"]').click()
+time.sleep(2)
+driver.switch_to.alert.accept()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # learn_seleniumpython
+https://www.linkedin.com/pulse/belajar-selenium-python-aria-uno-suseno/?originalSubdomain=id
